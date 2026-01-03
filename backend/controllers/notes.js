84a85b5729966133ac6bab50bb4e6adf3decfe03
Mariofhdez <mario.florez249@gmail.com>
2026-01-03 18:32:00 -0500

    Refactor get and post method due testing

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,60 +1,61 @@
-const notesRouter = require('express').Router();
-const Note = require('../models/note')
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
+notesRouter.get("/", async (req, res) => {
+  const notes = await Note.find({});
+  res.json(notes);
 });
 
-notesRouter.get('/:id', (req, res, next ) => {
-  const id = req.params.id
-  Note.findById(id).then(note => {
-      if(note){
-        res.json(note)
+notesRouter.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
       } else {
-        res.status(404).end()
+        res.status(404).end();
       }
-      
-    }).catch(error => next(error))
-  
-})
+    })
+    .catch((error) => next(error));
+});
 
-notesRouter.post('/', (req, res, next) => {
-  const body = req.body
-  
+notesRouter.post("/", (req, res, next) => {
+  const body = req.body;
+
   const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-  })
-  
-  note.save()
-    .then(savedNote => {
-        res.json(savedNote);
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      res.status(201).json(savedNote);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-notesRouter.delete('/:id', (req, res, next) =>{
-  const id = req.params.id
-  Note.findByIdAndDelete(id).then(() => {
-    res.status(204).end()
-  })
-  .catch(error => next(error))
-})
+notesRouter.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Note.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-notesRouter.put('/:id', (req, res, next) => {
-  const { content, important } = req.body
+notesRouter.put("/:id", (req, res, next) => {
+  const { content, important } = req.body;
 
   Note.findByIdAndUpdate(
-    req.params.id, 
-    {content, important}, 
-    { new: true, runValidators: true, context:'query' }
+    req.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
   )
-    .then(updatedNote => {
-      res.json(updatedNote)
+    .then((updatedNote) => {
+      res.json(updatedNote);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-module.exports = notesRouter
+module.exports = notesRouter;
--- backend/test/note_api.test.js
+++ backend/test/note_api.test.js
@@ -47,6 +47,41 @@ test('the first note is about HTTP methods', async() => {
     assert(contents.includes('HTML is easy'))
 })
 
+test('a valid note can be added', async () => {
+    const newNote = {
+        content: "async/await simplifies making async calls",
+        important: true
+    }
+
+    await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map(r => r.content)
+
+    assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+    assert(contents.includes(newNote.content))
+})
+
+test('note without content is not added', async () => {
+    const newNote = {
+        important: true
+    }
+
+    await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(400)
+
+    const response = await api.get('/api/notes')
+    assert.strictEqual(response.body.length, initialNotes.length)
+})
+
 after( async() => {
     await mongoose.connection.close()
 })
