fff40b29800f12bcd2cf8fbc855f16c39c254f7a
Mariofhdez <mario.florez249@gmail.com>
2026-01-13 21:59:14 -0500

    Testing buttons

--- frontend/src/test/note.test.jsx
+++ frontend/src/test/note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from '../components/Note'
 
 test('renders content', () => {
@@ -7,10 +8,27 @@ test('renders content', () => {
     important: true,
   }
 
-  render(<Note note={note} />)
+  const { container } = render(<Note note={note} />)
 
-  const element = screen.getByText(
+  const div = container.querySelector('.note')
+  expect(div).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
-  expect(element).toBeDefined()
+})
+
+test('renders content', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
