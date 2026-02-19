c92e64a503feefa8a276b669a01978b2a64eb70c
Mariofhdez <mario.florez249@gmail.com>
2026-02-18 22:46:40 -0500

    Adding fetch library. Adding GET and POST methods

--- front-redux/src/App.jsx
+++ front-redux/src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import NoteForm from "./components/noteForm";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
+import { setNotes } from "./reducers/noteReducer";
+import noteService from "./services/notes";
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+  })
   return (
     <div>
       <NoteForm />
