d07fc976054264e0320295e536b7dde96d6b8835
Mariofhdez <mario.florez249@gmail.com>
2026-02-21 00:19:22 -0500

    useReducer and context on react

---
+++ front-context/src/CounterContext.jsx
@@ -0,0 +1,29 @@
+import { useReducer } from "react";
+import { createContext } from "react";
+
+const counterReducer = (state, action) => {
+  switch(action.type){
+    case 'INC':
+      return state + 1
+    case 'DEC':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      state
+  }
+}
+
+const CounterContext = createContext()
+
+export const CounterContextProvider = (props) => {
+    const [counter, counterDispatch] = useReducer(counterReducer,0)
+
+    return(
+        <CounterContext.Provider value={{counter, counterDispatch}}>
+            {props.children}
+        </CounterContext.Provider>
+    )
+}
+
+export default CounterContext
