64a6f883cb8436ac1bd2e24f3b709593c81ce223
Mariofhdez <mario.florez249@gmail.com>
2026-02-27 12:17:26 -0500

    Setting environment for class-components

---
+++ class-components/src/index.js
@@ -0,0 +1,6 @@
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<>Hola mundo</>, document.getElementById('root'))
